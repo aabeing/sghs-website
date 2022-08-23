aa60b88577cd3d9e7567c8d832b9560459dedb67
aab <georgealways04u@gmail.com>
2022-08-23 13:50:10 +0530

    wideimages added

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -14,25 +14,15 @@ import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
 const images = [
     {
-        label: 'San Francisco – Oakland Bay Bridge, United States',
+        label: 'San Francisco- Oakland Bay Bridge, United States',
         imgPath:
-            'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+            '/images/w1.jpg',
     },
     {
         label: 'Bird',
         imgPath:
-            'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-        label: 'Bali, Indonesia',
-        imgPath:
-            'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
-    },
-    {
-        label: 'Goč, Serbia',
-        imgPath:
-            'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-    },
+        '/images/w2.jpg',
+    }
 ];
 
 function Carousel() {

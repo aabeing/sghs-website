7e384c6ff0aad4ccb0eefb30743764d67daf232b
aab <georgealways04u@gmail.com>
2022-08-23 08:59:37 +0530

    Carousel effect component added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import { Box, Divider, Grid, Paper } from '@mui/material'
 import React from 'react'
 import SchoolPic from '../../images/school.jpg'
+import Carousel from '../Carousel'
 
 
 {/* <img srcset="elva-fairy-320w.jpg,
@@ -19,18 +20,15 @@ function Home() {
     }}>
       <Grid container columns={12} >
         <Grid item xs={11.9} md={5.9} padding={2}>
-          <Paper elevation={1} sx={{
-            margin: 3, padding: 3
-          }}>
-            <Box component="img"
+            <Carousel/>
+            {/* <Box component="img"
               sx={{
                 height: '100%',
                 width: '100%',
                 maxHeight: { xs: '100%', md: '100%' },
                 maxWidth: { xs: '100%', md: '100%' },
               }} alt="The house from the offer." src={SchoolPic}
-            />
-          </Paper>
+            /> */}
         </Grid>
         
         <Grid item xs={0.2} md={0.2} container justifyContent='center' alignContent='center'>

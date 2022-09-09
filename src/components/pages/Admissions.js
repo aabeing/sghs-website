632222e6f06f6ec6d6af9fb196b3d7faef586ca1
aab <georgealways04u@gmail.com>
2022-09-09 15:14:25 +0530

    iframe tittle

--- src/components/pages/Admissions.js
+++ src/components/pages/Admissions.js
@@ -5,7 +5,7 @@ function Admissions() {
   // <iframe src="" >Loading…</iframe>
   return (
     <Box height="80vh">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSepvARM_Z_BpoJURksyCuB2esOByo_MNixKI24vMq7dLwtRSQ/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <iframe title='admission form' src="https://docs.google.com/forms/d/e/1FAIpQLSepvARM_Z_BpoJURksyCuB2esOByo_MNixKI24vMq7dLwtRSQ/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
       </Box>
   )
 }

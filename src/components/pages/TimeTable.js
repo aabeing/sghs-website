56d12a0beac8503805b58958d47d5f2b04108486
aab <georgealways04u@gmail.com>
2022-09-12 00:49:52 +0530

    timetable as iframe

--- src/components/pages/TimeTable.js
+++ src/components/pages/TimeTable.js
@@ -16,13 +16,14 @@ function TimeTable() {
     //     iframeDoc.document.getElementById(id).style.display = "none";
     // };
     const googleUrl = sanitize("https://docs.google.com/document/d/e/2PACX-1vQbbrQWQxx0bQlX8POPBoOABf5WwgPAvBnYd_XvW1JOJg5j5pVwhAUxwrbuSuzZsz7Ws3c8FS5jmB2W/pub?embedded=true");
+    const googleUrl2 = sanitize("https://docs.google.com/document/d/e/2PACX-1vQbbrQWQxx0bQlX8POPBoOABf5WwgPAvBnYd_XvW1JOJg5j5pVwhAUxwrbuSuzZsz7Ws3c8FS5jmB2W/pub")
 
     return (
         <>
             <Typography variant='h4' sx={{ mt: 5 }} align='center'>Time Table - {now.getFullYear()}</Typography>
-            <Grid container justifyContent='center'>
+            {/* <Grid container justifyContent='center'> */}
                 {/* <Grid item xs={2}></Grid> */}
-                <Grid item xs={6}>
+                {/* <Grid item xs={6}> */}
                     <Box height="70vh" alignContent='center' >
                         {/* <iframe src={sheetUrl} onLoad={iframeLoading}></iframe> */}
                         <iframe
@@ -32,7 +33,8 @@ function TimeTable() {
                             //     top: '50%',
                             //     // transform: 'translate(-50%,-50%)'
                             // }}
-                            title='Timetable sheet' src={googleUrl}
+                            title='Timetable sheet' 
+                            src={googleUrl2}
                             width="100%"
                             height="600"
                             // frameBorder="0" marginHeight="0" marginWidth="0"
@@ -40,8 +42,8 @@ function TimeTable() {
                             // frameBorder='none'
                             onLoad={iframeLoading}>Loading…</iframe>
                     </Box>
-                </Grid>
-            </Grid >
+                {/* </Grid> */}
+            {/* </Grid > */}
             {load ? <Loading /> : null}
         </>
     )
